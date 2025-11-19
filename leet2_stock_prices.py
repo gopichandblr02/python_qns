c6b5cd4543ba2ad3818c5e800ade5801b8805336
gopichandblr02 <gopichand.blr02@gmail.com>
2025-11-18 22:23:40 -0600

    new adds

--- leet2_stock_prices.py
+++ leet2_stock_prices.py
@@ -2,13 +2,11 @@ class Solution:
     def maxProfit(self,prices):
         if len(prices)<2:
             return 0
-        min_val=float('inf')
+        min_price=float('inf')
         max_profit=0
-        for x in prices:
-            if x<min_val:
-                min_val=x
-            elif max_profit < x-min_val:
-                max_profit=x-min_val
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
         return max_profit
 
 arr=[7,1,5,3,6,4]
