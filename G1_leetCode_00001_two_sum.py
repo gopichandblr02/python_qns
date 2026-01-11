c3db8a2e74d41ab122e6f95f44cabc55bb942cb2
gopichandblr02 <gopichand.blr02@gmail.com>
2026-01-10 22:28:20 -0600

    new adds

--- G1_leetCode_00001_two_sum.py
+++ G1_leetCode_00001_two_sum.py
@@ -8,5 +8,13 @@ class Solution:
             else:
                 seen[num]=i
 
+    def twoSum2(self, nums, target):
+        seen={}
+        for i,num in enumerate(nums):
+            rem = target-num
+            if rem in seen:
+                return [seen[rem],i]
+            seen[num]=i  # Just to show that it works without else as well
+
 sol = Solution()
 print(sol.twoSum([3,2,4],6))
