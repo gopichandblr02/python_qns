5c2e5c9872b8418f2d543212cdd36714f56ef99c
gopichandblr02 <gopichand.blr02@gmail.com>
2026-01-20 12:31:03 -0600

    new adds

--- Array_leetCode_9/26_remove_dups_sorted_array.py
+++ Array_leetCode_9/26_remove_dups_sorted_array.py
@@ -2,7 +2,6 @@ class Solution:
     def removeDuplicates(self, nums):
         if not nums:
             return 0
-
         i = 1
         for j in range(1, len(nums)):
             if nums[j] != nums[j - 1]:
