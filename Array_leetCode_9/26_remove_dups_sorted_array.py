15741892c2ee63f2211c5b91127b52dbaf57df3d
gopichandblr02 <gopichand.blr02@gmail.com>
2026-01-20 12:36:12 -0600

    new adds

--- Array_leetCode_9/26_remove_dups_sorted_array.py
+++ Array_leetCode_9/26_remove_dups_sorted_array.py
@@ -5,10 +5,9 @@ class Solution:
         i = 1
         for j in range(1, len(nums)):
             if nums[j] != nums[j - 1]:
-                # nums[i] = nums[j]
+                nums[i] = nums[j]
                 i += 1
-
-        return i
+        return i,nums
 
 
 sol = Solution()
