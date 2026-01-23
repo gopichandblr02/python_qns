20db0dfc6619bbd623b6d4cf977b59be15797450
gopichandblr02 <gopichand.blr02@gmail.com>
2026-01-22 22:16:56 -0600

    new adds

--- Array_leetCode_9/88_merge_sorted_array.py
+++ Array_leetCode_9/88_merge_sorted_array.py
@@ -1,9 +1,17 @@
+"""
+- You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+representing the number of elements in nums1 and nums2 respectively.
+- Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+- To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+"""
+
 class MergeSolution:
     def merge(self, nums1, m, nums2, n):
         i = m - 1
         j = n - 1
         k = m + n - 1
-
         while j >= 0:
             if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -20,3 +28,8 @@ nums2 = [2,5,6]
 n=3
 
 print(MergeSolution().merge(nums1,m,nums2,n))
+# [1, 2, 2, 3, 5, 6]
+print(MergeSolution().merge(nums1 = [1], m = 1, nums2 = [], n = 0))
+# Output: [1]
+print(MergeSolution().merge(nums1 = [0], m = 0, nums2 = [1], n = 1))
+# Output: [1]
