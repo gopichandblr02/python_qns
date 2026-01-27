c7876e56d3ba87f09aa93aa7b12e8326aa276b31
gopichandblr02 <gopichand.blr02@gmail.com>
2026-01-26 21:36:18 -0600

    new adds

---
+++ 1D_DP/70_Climbing_Stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        prev1 = 1  # ways to reach step 1
+        prev2 = 2  # ways to reach step 2
+
+        for _ in range(3, n + 1):
+            curr = prev1 + prev2
+            prev1 = prev2
+            prev2 = curr
+
+        return curr
+
+# Example usage:
+if __name__ == "__main__":
+    solution = Solution()
+    n = 5
+    print(f"Number of ways to climb {n} stairs: {solution.climbStairs(n)}")  #8
---
+++ 1D_DP/746_Min_Cost_Climbing_Stairs.py
@@ -0,0 +1,25 @@
+"""
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase.
+Once you pay the cost, you can either climb one or two steps.
+You can either start from the step with index 0, or the step with index 1.
+Return the minimum cost to reach the top of the floor.
+"""
+
+class Solution:
+    def minCostClimbingStairs(self, cost):
+        prev1=cost[0]
+        prev2=cost[1]
+        n=len(cost)
+        curr=0
+        for i in range(2,n):
+            curr=cost[i]+min(prev1,prev2)
+            prev1=prev2
+            prev2=curr
+        return min(prev1,prev2)
+
+
+# Example usage:
+if __name__ == "__main__":
+    solution = Solution()
+    cost = [10, 15, 20]
+    print(f"Minimum cost to climb stairs: {solution.minCostClimbingStairs(cost)}") #15
