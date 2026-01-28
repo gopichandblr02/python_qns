09bc08d80f14ed12816e4640bc4e54d0b20ab514
gopichandblr02 <gopichand.blr02@gmail.com>
2026-01-27 21:08:58 -0600

    new adds

--- 1D_DP/70_Climbing_Stairs.py
+++ 1D_DP/70_Climbing_Stairs.py
@@ -2,15 +2,13 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 2:
             return n
-
         prev1 = 1  # ways to reach step 1
         prev2 = 2  # ways to reach step 2
-
+        curr=0
         for _ in range(3, n + 1):
             curr = prev1 + prev2
             prev1 = prev2
             prev2 = curr
-
         return curr
 
 # Example usage:
